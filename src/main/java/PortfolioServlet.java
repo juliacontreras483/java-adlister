21c32818ea2736a86d5daa33eeb5be67609ddf7f
Julia Contreras <juliacontreras483@gmail.com>
2020-07-14 10:39:37 -0500

    resume/portfolio

--- src/main/java/PortfolioServlet.java
+++ src/main/java/PortfolioServlet.java
@@ -1,2 +1,8 @@
 public class PortfolioServlet {
 }
+
+//
+//**Bonus** Make a PortfolioServlet that is mapped to /portfolio. Use this content for the response:
+//        String content = "<h1>My Portfolio</h1>";
+//        content += "<p>my awesome portfolio</p> <br><hr><br>";
+//        content += "<a href='/resume'>My Resume</a>";
--- src/main/java/ResumeServlet.java
+++ src/main/java/ResumeServlet.java
@@ -1,2 +1,29 @@
-public class ResumeServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet (name = "ResumeServlet", urlPatterns = "/Resume")
+public class ResumeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+
+        String content = "<h1>My Resume</h1>";
+        content += "<p>I write a lot of code. Check my GitHub</p>";
+        content += "<hr><br>";
+        content += "<a href='https://github.com/'>My GitHub Profile</a><br>";
+        content += "<a href='/portfolio'>My Portfolio</a>";
+
+        PrintWriter out = response.getWriter();
+        out.println(content);
+    }
 }
+
+
+
