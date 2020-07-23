4f37f235a264c5777cb31989735db6f46c9250d4
Julia Contreras <juliacontreras483@gmail.com>
2020-07-23 15:05:35 -0500

    passwords and fixing up other code

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -1,8 +1,5 @@
 package com.codeup.adlister.controllers;
 
-import com.codeup.adlister.dao.DaoFactory;
-import com.codeup.adlister.models.User;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,17 +20,15 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        User user = DaoFactory.getUsersDao().findByUsername(username);
 
-        if (user == null) {
-            response.sendRedirect("/login");
-            return;
-        }
-
-        boolean validAttempt = password.equals(user.getPassword());
+        // TODO: find a record in your database that matches the submitted password
+        // TODO: make sure we find a user with that username
+        // TODO: check the submitted password against what you have in your database
+        boolean validAttempt = false;
 
         if (validAttempt) {
-            request.getSession().setAttribute("user", user);
+            // TODO: store the logged in user object in the session, instead of just the username
+            request.getSession().setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -2,7 +2,6 @@ package com.codeup.adlister.dao;
 
 public class DaoFactory {
     private static Ads adsDao;
-    private static Users usersDao;
     private static Config config = new Config();
 
     public static Ads getAdsDao() {
@@ -10,12 +9,5 @@ public class DaoFactory {
             adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
-    }
-
-    public static Users getUsersDao() {
-        if (usersDao == null) {
-            usersDao = new MySQLUsersDao(config);
-        }
-        return usersDao;
     }
 }
